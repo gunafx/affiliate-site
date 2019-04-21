7f8b9aabbaf28efe1b1d693676f1ba5dc6c5fb62
Gunasekaran R <gunasekaran.r@thetechchefs.in>
2019-04-21 11:42:03 +0530

    discount & coupons dynamic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
 var globalData = {};
-var tipsSheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/1ysA699MyXQSJJlSyMwcVsLyV71qSGQXLHYlc8ITwO5k/values/tips?key=AIzaSyDarLhb6rGyePmRz2oHAltaZSQYjElyATQ";
+var sheetKey = "1ysA699MyXQSJJlSyMwcVsLyV71qSGQXLHYlc8ITwO5k";
+var apiKey = "AIzaSyDarLhb6rGyePmRz2oHAltaZSQYjElyATQ";
+var sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/"+sheetKey+"/values/";
 
 var tipsElm = "tips-list";
-var tipsTemplate = $("#"+tipsElm).html();
+var discountElm = "discount-list";
+var couponElm = "coupon-list";
+
 
 //update tips data
-getsheetData(tipsSheetUrl).then(function(data){
-	var tempStr = updateTemplate(tipsTemplate, data),
+getsheetData(sheetUrl+"tips").then(function(data){
+	var tempStr = updateTemplate($("#"+tipsElm).html(), data),
 		tempElm = document.getElementById(tipsElm);
 
 	tempElm.innerHTML = tempStr.join(" ");
 });
 
+//discount data
+getsheetData(sheetUrl+"discounts").then(function(data){
+	var tempStr = updateTemplate($("#"+discountElm).html(), data),
+		tempElm = document.getElementById(discountElm);
+
+	tempElm.innerHTML = tempStr.join(" ");
+});
+
+//discount data
+getsheetData(sheetUrl+"coupons").then(function(data){
+	var tempStr = updateTemplate($("#"+couponElm).html(), data),
+		tempElm = document.getElementById(couponElm);
+
+	tempElm.innerHTML = tempStr.join(" ");
+});
+
+
+
+
 function getsheetData(url) {
 	var getProm = new Promise(function(resolve, reject) {
-		return $.get(url,function(data) {
+		return $.get(url+"?key="+apiKey,function(data) {
 			var arr = [], val = data.values, feilds = data.values[0];
 			for(var i=1; i < val.length; i++) {
 				var row = {};
